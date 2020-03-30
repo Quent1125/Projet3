8250b8218855f83be5bd1b283c849d8b8c6a1271
Quentin <quentin.muzel@etu.univ-lyon1.fr>
2020-03-30 12:49:33 +0200

    test collapse

---
+++ js/acceuil.js
@@ -0,0 +1,13 @@
+document.getElementById('actu').onclick = function () {
+    document.getElementById('collapseTwi').style.display = "none";
+    document.getElementById('collapseFb').style.display = "none";
+}
+
+document.getElementById('twitter').onclick = function () {
+    document.getElementById('collapseActu').style.display = "none";
+    document.getElementById('collapseFb').style.display = "none";
+}
+
+document.getElementById('facebook').onclick = function () {
+    document.getElementById('collapseTwi').style.display = "none";
+    document.getElementById('collapseactu').style.display = "none";
